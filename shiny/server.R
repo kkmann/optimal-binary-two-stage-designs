264c0f7e55550424dc0a8cf9d320a4c3bf01ad52
Kevin Kunzmann <kevin.kunzmann@mrc-bsu.cam.ac.uk>
2020-01-22 22:42:17 +0000

    ..

--- shiny/server.R
+++ shiny/server.R
@@ -10,26 +10,26 @@ server <- function(input, output) {
         (input$epsilon * badr::Beta(1, 1) + (1 - input$epsilon) * badr::Beta(input$a, input$b)) <= input$pmp
     })
 
-    output$distPlot <- renderPlot({
+    output$plt_prior <- renderPlot({
         p <- seq(0, 1, .001)
         plot(p, density(prior(), p))
     })
 
-    design <- reactive({
-        input$optimise
-        isolate(
-            Problem(
-                minimise(SampleSize(prior())),
-                Power(prior() %|% input$p0) <= .05,
-                Power(prior() >= input$mrv) >= 1 - .2,
-                unimodal = input$unimodal
-            ) %>%
-            optimise_design()
-        )
-    })
+    # design <- reactive({
+    #     input$optimise
+    #     isolate(
+    #         Problem(
+    #             minimise(SampleSize(prior())),
+    #             Power(prior() %|% input$p0) <= .05,
+    #             Power(prior() >= input$mrv) >= 1 - .2,
+    #             unimodal = input$unimodal
+    #         ) %>%
+    #         optimise_design()
+    #     )
+    # })
 
-    output$designPlot <- renderPlot({
-        plot_compare_designs(design(), annotations = c(input$p0, input$mrv))
-    })
+    # output$plt_design <- renderPlot({
+    #     plot_compare_designs(design(), annotations = c(input$p0, input$mrv))
+    # })
 
 }
--- shiny/ui.R
+++ shiny/ui.R
@@ -4,57 +4,65 @@ ui <- navbarPage("",
         h1("bla")
     ),
 
-    tabPanel("Try it out!",
-        # Sidebar with a slider input for number of bins
+    tabPanel("Expected Power",
         sidebarLayout(
             sidebarPanel(
+                actionButton("optimise", "optimise", width = "100%"),
+                hr(),
+                h4("Informative beta prior component"),
+                p("define the parameters of an informative prior"),
                 fluidRow(
                 column(6, sliderInput("a",
-                            "a",
+                            withMathJax("$$a$$"),
                             min = 1,
                             max = 25,
                             value = 5)),
                 column(6, sliderInput("b",
-                            "b",
+                            withMathJax("$$b$$"),
                             min = 1,
                             max = 25,
                             value = 10))
                 ),
+                h4("Null hypothesis and relevance threshold"),
+                p("specify the upper boundary of the null hypothesis and the minimal clinically relevant response probability (expected power is only averaged over relevance region, i.e. probabilities larger than the minimal relevant value)"),
                 fluidRow(
                     column(6, sliderInput("p0",
-                                          "response probability under the null",
+                                          withMathJax("$$p_0$$"),
                                           min = .1,
                                           max = .9,
                                           value = .2, step = .1)
                     ),
                     column(6, sliderInput("mrv",
-                                          "MRV",
+                                          withMathJax("$$p_{MRV}$$"),
                                           min = .1,
                                           max = .9,
                                           value = .3, step = .1)
                     )
                 ),
+                h4("Prior robustification"),
+                p("specify the weight of the non-informative uniform prior component and the maximal plausible response probability."),
                 fluidRow(
                     column(6, sliderInput("epsilon",
-                                          "non-informative weight",
+                                          withMathJax("$$\\epsilon$$"),
                                           min = .0,
                                           max = 1.0,
                                           value = .2, step = .05)
                     ),
                     column(6, sliderInput("pmp",
-                                          "maximal plausible response probability",
+                                          withMathJax("$$p_{max}$$"),
                                           min = .1,
                                           max = 1.0,
                                           value = .7, step = .1)
                     )
                 ),
-                checkboxInput("unimodal", "force unimodal?", value = FALSE),
-                actionButton("optimise", "optimise")
+                h4("Unimodal sample size function?"),
+                p("check this box to force the solution to have a unimodal sample size function; note: this increases the runtime."),
+                checkboxInput("unimodal", "force unimodal?", value = FALSE)
             ),
 
             mainPanel(
-                plotOutput("distPlot"),
-                plotOutput("designPlot")
+                plotOutput("plt_prior"),
+                plotOutput("plt_design")
             )
         )
     )
