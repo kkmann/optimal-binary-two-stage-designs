8adce907f79b0b06eeb76537f4e0625ba22d71f7
Kevin Kunzmann <kevin.kunzmann@mrc-bsu.cam.ac.uk>
2020-01-22 23:20:40 +0000

    ..

--- shiny/server.R
+++ shiny/server.R
@@ -1,10 +1,13 @@
 library(shiny)
+library(badr)
+
+if (dir.exists("/home/jovyan/julia-1.3.1/bin")) options(JULIA_HOME = "/home/jovyan/julia-1.3.1/bin")
 
 tmp <- capture.output({
-    library(badr)
-    badr::load_julia_package()
-},
-type = "message")
+        badr::load_julia_package()
+    },
+    type = "message"
+)
 
 
 server <- function(input, output) {
@@ -20,21 +23,21 @@ server <- function(input, output) {
         plot(p, density(prior(), p))
     })
 
-    # design <- reactive({
-    #     input$optimise
-    #     isolate(
-    #         Problem(
-    #             minimise(SampleSize(prior())),
-    #             Power(prior() %|% input$p0) <= .05,
-    #             Power(prior() >= input$mrv) >= 1 - .2,
-    #             unimodal = input$unimodal
-    #         ) %>%
-    #         optimise_design()
-    #     )
-    # })
+    design <- reactive({
+        input$optimise
+        isolate(
+            Problem(
+                minimise(SampleSize(prior())),
+                Power(prior() %|% input$p0) <= .05,
+                Power(prior() >= input$mrv) >= 1 - .2,
+                unimodal = input$unimodal
+            ) %>%
+            optimise_design()
+        )
+    })
 
-    # output$plt_design <- renderPlot({
-    #     plot_compare_designs(design(), annotations = c(input$p0, input$mrv))
-    # })
+    output$plt_design <- renderPlot({
+        plot_compare_designs(design(), annotations = c(input$p0, input$mrv))
+    })
 
 }
