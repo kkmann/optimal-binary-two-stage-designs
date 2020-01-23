0e7089c9e4b35be31e0d303e96aa5be47adc5142
Kevin Kunzmann <kevin.kunzmann@mrc-bsu.cam.ac.uk>
2020-01-23 00:40:07 +0000

    ...

--- shiny/server.R
+++ shiny/server.R
@@ -3,11 +3,8 @@ library(badr)
 
 if (dir.exists("/home/jovyan/julia-1.3.1/bin")) options(JULIA_HOME = "/home/jovyan/julia-1.3.1/bin")
 
-tmp <- capture.output({
-        badr::load_julia_package()
-    },
-    type = "message"
-)
+badr::load_julia_package()
+
 
 
 server <- function(input, output) {
@@ -16,28 +13,46 @@ server <- function(input, output) {
         (input$epsilon * badr::Beta(1, 1) + (1 - input$epsilon) * badr::Beta(input$a, input$b)) <= input$pmp
     })
 
-    output$tmp <- renderText(tmp)
-
     output$plt_prior <- renderPlot({
         p <- seq(0, 1, .001)
         plot(p, density(prior(), p))
     })
 
-    design <- reactive({
+    design_ep <- reactive({withProgress(message = 'optimising expected power...', value = 1, {
         input$optimise
         isolate(
             Problem(
                 minimise(SampleSize(prior())),
-                Power(prior() %|% input$p0) <= .05,
-                Power(prior() >= input$mrv) >= 1 - .2,
-                unimodal = input$unimodal
+                Power(prior() %|% input$p0mrv[1]) <= .05,
+                Power(prior() >=  input$p0mrv[2]) >= 1 - .2,
+                unimodal = input$unimodal,
+                label = 'EP design'
             ) %>%
             optimise_design()
         )
+    })})
+
+    output$plt_ep_design <- renderPlot({
+        plot_compare_designs(design_ep(), annotations = input$p0mrv)
     })
 
-    output$plt_design <- renderPlot({
-        plot_compare_designs(design(), annotations = c(input$p0, input$mrv))
+    design_minimax <- reactive({withProgress(message = 'optimising minimax...', value = 1, {
+        input$optimise_minimax
+        isolate(
+            Problem(
+                minimise_maximal_sample_size(.2, PointMass(input$p01_minimax[2])),
+                Power(prior() %|% input$p01_minimax[1]) <= .05,
+                Power(prior() %|% input$p01_minimax[2]) >= 1 - .2,
+                unimodal = input$unimodal,
+                label = 'minimax design',
+                maxmultipleonestage = 1.0
+            ) %>%
+            optimise_design()
+        )
+    })})
+
+    output$plt_minimax_design <- renderPlot({
+        plot_compare_designs(design_minimax(), annotations = input$p01_minimax)
     })
 
 }
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,8 +1,7 @@
 ui <- navbarPage("",
 
     tabPanel("Background",
-        h1("bla"),
-        textOutput("tmp")
+        h1("bla")
     ),
 
     tabPanel("Expected Power",
@@ -26,19 +25,11 @@ ui <- navbarPage("",
                 ),
                 h4("Null hypothesis and relevance threshold"),
                 p("specify the upper boundary of the null hypothesis and the minimal clinically relevant response probability (expected power is only averaged over relevance region, i.e. probabilities larger than the minimal relevant value)"),
-                fluidRow(
-                    column(6, sliderInput("p0",
-                                          withMathJax("$$p_0$$"),
-                                          min = .1,
-                                          max = .9,
-                                          value = .2, step = .1)
-                    ),
-                    column(6, sliderInput("mrv",
-                                          withMathJax("$$p_{MRV}$$"),
-                                          min = .1,
-                                          max = .9,
-                                          value = .3, step = .1)
-                    )
+                sliderInput("p0mrv",
+                    withMathJax("$$p_0 \\quad \\text{and} \\quad p_{MRV}$$"),
+                    min = .1, max = .9, step = .05,
+                    value = c(.2, .3)
+
                 ),
                 h4("Prior robustification"),
                 p("specify the weight of the non-informative uniform prior component and the maximal plausible response probability."),
@@ -63,8 +54,33 @@ ui <- navbarPage("",
 
             mainPanel(
                 plotOutput("plt_prior"),
-                plotOutput("plt_design")
+                plotOutput("plt_ep_design")
             )
         )
+    ),
+
+    tabPanel("MiniMax",
+             sidebarLayout(
+                 sidebarPanel(
+                     actionButton("optimise_minimax", "optimise", width = "100%"),
+                     hr(),
+                     h4("Null and alternative hypotheses"),
+                     p("specify the upper boundary of the null hypothesis and the minimal clinically relevant response probability (expected power is only averaged over relevance region, i.e. probabilities larger than the minimal relevant value)"),
+                     sliderInput("p01_minimax",
+                                 withMathJax("$$p_0 \\quad \\text{and} \\quad p_{alt}$$"),
+                                 min = .1, max = .9, step = .05,
+                                 value = c(.2, .4)
+
+                     ),
+                     h4("Unimodal sample size function?"),
+                     p("check this box to force the solution to have a unimodal sample size function; note: this increases the runtime."),
+                     checkboxInput("unimodal_minimax", "force unimodal?", value = FALSE)
+                 ),
+
+                 mainPanel(
+                     plotOutput("plt_minimax_design")
+                 )
+             )
     )
+
 )
